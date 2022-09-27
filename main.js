dfcbb480b68763c0ea753e344060705eaed58d6b
Ppcalatayud <pepe2003.pc@gmail.com>
2022-09-27 20:30:36 +0200

    primer commit

---
+++ main.js
@@ -0,0 +1,37 @@
+// 1. Booleanos
+
+const booleano1 = true
+const booleano2 = false
+
+const booleanoAnd = booleano1 && booleano2
+
+const boleanoOr = booleano1 || booleano2
+
+const booleanoNot = !booleano1
+
+const booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2))
+
+
+
+// 2.Operadores
+
+const valorDivision = (17 / 3).toFixed(2)
+const valorResto = 17%7
+ 
+
+
+// 3.Lógica de programación
+
+const a = 9;
+const b = "9";
+
+if (a == b)
+    console.log("Son iguales")
+else {
+
+    console.log("No iguales")
+}
+
+
+// no se seguir
+
